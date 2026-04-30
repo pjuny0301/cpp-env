16b17ccb9fef541eaf1f0373301b945e72219ff1
pjuny0301 <pjuny0301@users.noreply.github.com>
2026-04-30 13:12:28 +0900

    Include asset bytes provider in aggregate contracts

--- apps/quiz/quiz-vulkan/tests/contracts/interface_contract_compile_tests.cpp
+++ apps/quiz/quiz-vulkan/tests/contracts/interface_contract_compile_tests.cpp
@@ -4,6 +4,7 @@
 #include "../audio/audio_engine_interface_contract_tests.cpp"
 #include "../app/app_render_pipeline_interface_contract_tests.cpp"
 #include "../app/app_sound_router_interface_contract_tests.cpp"
+#include "../assets/asset_bytes_provider_interface_contract_tests.cpp"
 #include "../assets/asset_manifest_interface_contract_tests.cpp"
 #include "../assets/asset_runtime_catalog_interface_contract_tests.cpp"
 #include "../assets/deck_source_asset_adapter_interface_contract_tests.cpp"
